621b22e2192b466bf03ed35b9f83a88d9f2b67e4
ChengyuXue <cxue23@outlook.com>
2024-03-03 02:31:27 +0000

    Added brief comment

--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,31 +9,27 @@ namespace CMP1903_A1_2324
 {
     internal class Game
     {
-        private static Random random = new Random();
+        private Die[] dice = new Die[3];
 
-        private static int RollDie()
+        // Constructor
+        public Game()
         {
-            return random.Next(1,7)
-        }
-
-        public static int RollThreeDice()
-        {
-            int total = 0:
-            for(int i = 0; i < 3; i++)
+            for (int i = 0; i < dice.Length; i++)
             {
-                total +=RollDie()
+                dice[i] = new Die();
             }
-            return total
         }
 
-        /*
-         * The Game class should create three die objects, roll them, sum and report the total of the three dice rolls.
-         *
-         * EXTRA: For extra requirements (these aren't required though), the dice rolls could be managed so that the
-         * rolls could be continous, and the totals and other statistics could be summarised for example.
-         */
-
-        //Methods
-
+        // Method to roll three dice, sum their values, and report the total
+        public int RollDice()
+        {
+            int total = 0;
+            for (int i = 0; i < dice.Length; i++)
+            {
+                total += dice[i].Roll();
+            }
+            Console.WriteLine($"Total of three dice rolls: {total}");
+            return total;
+        }
     }
 }
