72ca173ccb7e06bfcb4c2d20d43a5caa05b696b6
ChengyuXue <cxue23@outlook.com>
2024-03-06 01:37:02 +0000

    Updated to return the game object after testing

--- CMP1903_A1_2324/Testing.cs
+++ CMP1903_A1_2324/Testing.cs
@@ -10,17 +10,20 @@ namespace CMP1903_A1_2324
     internal class Testing
     {
         // Method to test the Die and Game classes
-        public static void Test()
+        public static Game Test()
         {
             // Test Die class
             Die die = new Die();
             int rollValue = die.Roll();
-            Debug.Assert(rollValue >= 1 && rollValue <= 6, "Die roll should be between 1 and 6");
+            Debug.Assert(rollValue >= 1 && rollValue <= 6, "Error: Die roll should be between 1 and 6");
 
             // Test Game class
             Game game = new Game();
             int sum = game.RollDice();
-            Debug.Assert(sum >= 3 && sum <= 18, "Sum of three dice should be between 3 and 18");
+            Debug.Assert(sum >= 3 && sum <= 18, "Error: Sum of three dice should be between 3 and 18");
+
+            // Return the game object for use in roll dice continuously
+            return game;
         }
     }
 }
