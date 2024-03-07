72efa2425c93a60a143967f55fb8bf72b330ed4c
ChengyuXue <cxue23@outlook.com>
2024-03-07 14:09:38 +0000

    Update Testing.cs

--- CMP1903_A1_2324/Testing.cs
+++ CMP1903_A1_2324/Testing.cs
@@ -12,15 +12,20 @@ namespace CMP1903_A1_2324
         // Method to test the Die and Game classes
         public static Game Test()
         {
+            Console.WriteLine("Testing Die class and Game class");
+
             // Test Die class
             Die die = new Die();
             int rollValue = die.Roll();
             Debug.Assert(rollValue >= 1 && rollValue <= 6, "Error: Die roll should be between 1 and 6");
+            Console.WriteLine("Die class test passed");
+
 
             // Test Game class
             Game game = new Game();
             int sum = game.RollDice();
             Debug.Assert(sum >= 3 && sum <= 18, "Error: Sum of three dice should be between 3 and 18");
+            Console.WriteLine("Game class test passed");
 
             // Return the game object for use in roll dice continuously
             return game;
